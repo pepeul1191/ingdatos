234c013fb3bb3d630d7d99d64063ff51845410b9
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2022-11-03 09:19:09 -0500

    hola mundo bottle

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from bottle import Bottle, run
+
+app = Bottle()
+
+@app.route('/', method='GET')
+def home():
+  return 'hola mundo'
+
+if __name__ == '__main__':
+  run(
+    app, 
+    host='0.0.0.0', 
+    port=8081, 
+    debug=True, 
+    reloader=True
+  )
